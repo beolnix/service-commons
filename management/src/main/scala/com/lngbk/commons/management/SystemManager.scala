dfa15fbcd1d5ee54ef808935f6709af86cd33cb1
beolnix <beolnix@gmail.com>
2016-09-11 22:43:38 +0200

    bug fix

--- management/src/main/scala/com/lngbk/commons/management/SystemManager.scala
+++ management/src/main/scala/com/lngbk/commons/management/SystemManager.scala
@@ -2,15 +2,31 @@ package com.lngbk.commons.management
 
 import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
+import org.slf4j.LoggerFactory
 
 /**
   * Created by beolnix on 09/09/16.
   */
 object SystemManager {
 
-  private val _config = ConfigFactory.load("akka-application")
-  private val _system = ActorSystem("LngbkSystem", _config)
+  private val logger = LoggerFactory.getLogger(SystemManager.getClass)
 
-  def system = _system
+  private val _config = ConfigFactory.load("akka-application")
+  private var _system = Option.empty[ActorSystem]
+
+  def initWithSystem(embeddedSystem: ActorSystem): Unit = {
+    logger.info(s"Init system with $embeddedSystem")
+    _system = Some(embeddedSystem)
+  }
+
+  def system = {
+    if (_system.nonEmpty) {
+      _system.get
+    } else {
+      logger.info(s"Init default system")
+      _system = Some(ActorSystem("LngbkSystem", _config))
+      _system.get
+    }
+  }
 
 }
